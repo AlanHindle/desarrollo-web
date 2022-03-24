cb9e290cd071dd67f91b1808999825834e7f2a68
Maulan Digital <info@maulandigital.com>
2022-03-24 12:28:45 -0300

    Inner HTML

--- assets/js/simulador-prestamo/app.js
+++ assets/js/simulador-prestamo/app.js
@@ -1,10 +1,21 @@
+// Esta linea dice que cuando cargue la pagina por complete (el html) entonces ejecutara la funcion "load()"
+window.onload = load;
+
+function load(){
+    // Busco el boton con id="btn_onboarding" usando document.getElementById("btn_onboarding") y le agrego un evento "click"
+    // Entonces cuando se haga click sobre el boton indicado, llamara a la funcion "cotizarNuevoPrestamo"
+    document.getElementById("btn_onboarding").addEventListener("click", cotizarNuevoPrestamo);
+}
+
 function cotizarNuevoPrestamo() {
 
-    solicitudDePrestamo = parseInt(prompt("Ingrese el valor del prestamo"))
+    // Comento esta linea, puesto que ahora usare el input range del monto. Lo buscamos por su id "SimuladorMontoId"
+    //solicitudDePrestamo = parseInt(prompt("Ingrese el valor del prestamo"))
+    solicitudDePrestamo = parseInt(document.getElementById("SimuladorMontoId").value); // La propiedad "value" me devuelve el valor actual del elemento input. 
 
     if (solicitudDePrestamo <= 300) {
-
-        diaDeDevolucion = parseInt(prompt("Ingrese cantidad de tiempo a devolver:"))
+        // Al igual que con el monto, busco el valor de los dias del input range mediante su id "SimuladorDiasId"
+        diaDeDevolucion = parseInt(document.getElementById("SimuladorDiasId").value);
 
         if (diaDeDevolucion <= 30) {
             const cotizarNuevoPrestamo = new Prestamo(solicitudDePrestamo, diaDeDevolucion, interesDelPrestamo)
@@ -18,13 +29,30 @@ function cotizarNuevoPrestamo() {
             console.log("Fecha de devolución: " + resDiasPrestamo + " días")
             console.log("Total a devolver: " + res + " €")
 
+            // Con document.getElementById("...") obtengo ahora los elemento a los que le voy a añadir texto, al igual que en el console.log
+            // La propiedad "innerText" me permite escribir texto dentro de las etiquetas del elemento.
+            document.getElementById("pInteresesId").innerText = interesDelPrestamo.toString() + " %";
+            document.getElementById("SimuladorOutputImpPrestamoId").innerText = resPrestamo.toString() + " €";
+            document.getElementById("SimuladorOutputFechaDevId").innerText = resDiasPrestamo.toString() + " días";
+            document.getElementById("SimuladorOutputTotalDevId").innerText = res.toString() + " €";
+
         } else {
             console.log("Debe ingresar menos de 30 días")
-            cotizarNuevoPrestamo()
+            alert("Debe ingresar menos de 30 días"); // Te recomiendo usar un modal de bootstrap para que quede mas atractivo. Le puse alert para hacerlo rapido.
+
+            // Se comenta el volver a llamar la funcion, puesto que ahora la funcion se llamaria cada vez que el usuario le de click al boton btn_onboarding
+            // Si no se comenta esta recursividad, entonces habria un bucle infinto, puesto que ya no hay una espera de los valores de monto y dia, ya que estos los saca
+            // de los elementos "input range"
+            //cotizarNuevoPrestamo()
         }
 
     } else {
         console.log("Debe ingresar un monto menor a 300 Euros")
-        cotizarNuevoPrestamo()
+        alert("Debe ingresar un monto menor a 300 Euros"); // Te recomiendo usar un modal de bootstrap para que quede mas atractivo. Le puse alert para hacerlo rapido.
+
+        // Se comenta el volver a llamar la funcion, puesto que ahora la funcion se llamaria cada vez que el usuario le de click al boton btn_onboarding
+        // Si no se comenta esta recursividad, entonces habria un bucle infinto, puesto que ya no hay una espera de los valores de monto y dia, ya que estos los saca
+        // de los elementos "input range"
+        //cotizarNuevoPrestamo()
     }
 }
--- assets/js/simulador-prestamo/variables.js
+++ assets/js/simulador-prestamo/variables.js
@@ -1,3 +1,6 @@
 let solicitudDePrestamo = 0
 let diaDeDevolucion = 0
 const interesDelPrestamo = 30
+
+
+contenedorFechaDevolucionPrestamo.innerText = "FECHA"
